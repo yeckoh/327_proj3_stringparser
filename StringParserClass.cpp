4ee0fbd150f9ada0c3b7c3b649b9d4ef449c8e0a
yeckoh <yeckoh@gmail.com>
2019-10-26 09:17:38 -0700

    fix getDataBtwnTags

--- StringParserClass.cpp
+++ StringParserClass.cpp
@@ -1,22 +1,21 @@
-/*
- * StringParserClass.cpp
- *
- *  Created on: Oct 8, 2017
- *      Author: keith
- */
+/*============================================================================
+Name        : StringParserClass.cpp
+Author      : wchang 00960978
+Prof        : K. Perkins @ CNU
+Version     : 26 Oct 2k19
+Project	    : cs327 - p4: classes, pointers, libraries
+testing args: "../data/testdata_full.txt" "<to>" "</to>" "../output/outfile.txt"
+============================================================================*/
 
 #include <string>
 #include <string.h>
 #include <vector>
-#include <sstream>
-#include <iostream>
 #include "../327_proj3_test/includes/StringParserClass.h"
 #include "../327_proj3_test/includes/constants.h"
 
 using namespace std;
 using namespace KP_StringParserClass;
 
-		// constructor
 		StringParserClass::StringParserClass() {
 			pStartTag = NULL;
 			pEndTag = NULL;
@@ -24,19 +23,10 @@ using namespace KP_StringParserClass;
 
 		}
 
-		//call cleanup to release any allocated memory
-		// destructor
 		StringParserClass::~StringParserClass() {
 			cleanup();
 		}
 
-		//these are the start tag and the end tags that we want to find,
-		//presumably the data of interest is between them, please make a
-		//COPY of what pStartTag and pEndTag point to.  In other words
-		//DO NOT SET pStartTag = pStart
-		//returns:
-		//SUCCESS
-		//ERROR_TAGS_NULL if either pStart or pEnd is null
 		int StringParserClass::setTags(const char *pStart, const char *pEnd) {
 			if(!pStart || !pEnd)
 				return ERROR_TAGS_NULL;
@@ -49,49 +39,33 @@ using namespace KP_StringParserClass;
 			pEndTag = new char[length];
 			strcpy(pEndTag, pEnd);
 
-			//pStartTag = new char(*pStart); // this works on VS_2017
-			//pEndTag = new char(*pEnd);
 			areTagsSet = true;
 			return SUCCESS;
 		}
 
-		//First clears myVector
-		//going to search thru pDataToSearchThru, looking for info bracketed by
-		//pStartTag and pEndTag, will add that info only to myVector
-		//returns
-		//SUCCESS  finished searching for data between tags, results in myVector (0 or more entries)
-		//ERROR_TAGS_NULL if either pStart or pEnd is null
-		//ERROR_DATA_NULL pDataToSearchThru is null
 		int StringParserClass::getDataBetweenTags(char *pDataToSearchThru, vector<string> &myVector) {
 			myVector.clear();
-			if(!areTagsSet)	// confirm order of fail conditions during testing
+			if(!areTagsSet)	// order confirmed for smash
 				return ERROR_TAGS_NULL;
 			if(!pDataToSearchThru)
 				return ERROR_DATA_NULL;
 
 			string data = pDataToSearchThru;
+			int start = 0;
+			int end = 0;
 
-			// take all data and get desired subset
-			int start = data.find(pStartTag) + strlen(pStartTag);
-			int end = data.find(pEndTag);
-			data = data.substr(start, (end-start));
-			stringstream ss(data);
-
-			// remove newlines
-			// happens in fileIO already, Oof
-
-			// add tokens to vector
-			string line = "";
-			while(ss >> line) {
-				myVector.push_back(line);
+			while (start != string::npos) {
+				start = data.find(pStartTag, start + 1);
+				end = data.find(pEndTag, start + 1);
+				if (start != string::npos & end != string::npos)
+					myVector.push_back(data.substr(start + strlen(pStartTag), (end - (start + strlen(pStartTag)))));
 			}
-
 			return SUCCESS;
 		}
 
 
 		void StringParserClass::cleanup() {
-			if(areTagsSet){
+			if(areTagsSet) {
 				delete[] pStartTag;
 				delete[] pEndTag;
 			}
@@ -99,25 +73,19 @@ using namespace KP_StringParserClass;
 			areTagsSet = 0;
 		}
 
-		//Searches a string starting at pStart for pTagToLookFor
-		//returns:
-		//SUCCESS  found pTagToLookFor, pStart points to beginning of tag and pEnd points to end of tag
-		//FAIL did not find pTagToLookFor and pEnd points to 0
-		//ERROR_TAGS_NULL if either pStart or pEnd is null
+
 		int StringParserClass::findTag(char *pTagToLookFor, char *&pStart, char *&pEnd) {
+			// this function never gets used/tested
+			// return values make it a useless helper function
 			if(!pStart || !pEnd)
 				return ERROR_TAGS_NULL;
 
 			string look_in = pStart;
-			unsigned int finder = look_in.find(pTagToLookFor);
+			int finder = look_in.find(pTagToLookFor);
 			if(finder == string::npos)
 				return FAIL;
-			*pStart = *pStart + finder;
+			pStart = pStart + finder;
 			pEnd = pStart + strlen(pTagToLookFor);
 			return SUCCESS;
 		}
 
-
-
-
-		// args: "../data/testdata_full.txt" "<to>" "</to>" "../output/outfile.txt"
